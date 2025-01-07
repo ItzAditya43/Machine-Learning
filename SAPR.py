3c4226146b97a2b3a47bebe6b1bf214b48c4c1c3
kerryyyy <adikun10723@gmail.com>
2025-01-07 19:49:06 +0530

    Update SAPR.py

--- SAPR.py
+++ SAPR.py
@@ -3,8 +3,6 @@ import pandas as pd
 df = pd.read_csv('Amazon-Product-Reviews.csv')
 
 print(df.head())
-print(df.info())
-print(df.describe())
 
 # not getting the part on how should I proceed with cleaning and preprocessing the data
 # I will try to do it myself after some more learning
