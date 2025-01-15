c5a133b4a4760a0e49ecc8442477e25b896ccec7
kerryyyy <adikun10723@gmail.com>
2025-01-15 14:25:11 +0530

    Learned Data Preprocessing, Data Visualization, Model Building, Model Evaluation in this.

--- SAPR.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('Amazon-Product-Reviews.csv')
-
-print(df.head())
-
-# not getting the part on how should I proceed with cleaning and preprocessing the data
-# I will try to do it myself after some more learning
-#Link of dataset: https://www.kaggle.com/datasets/miriamodeyianypeter/sentiment-analysis-amazon-product-reviews
